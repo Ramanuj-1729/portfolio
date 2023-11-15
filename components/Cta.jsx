1f6dfb690504558c5300fb963d1310fc0a4872c9
Ramanuj <75874452+Ramanuj-1729@users.noreply.github.com>
2023-11-15 23:12:38 +0530

    create cta and footer section

---
+++ components/Cta.jsx
@@ -0,0 +1,19 @@
+import { Button } from "./ui/button";
+import Link from "next/link";
+
+const Cta = () => {
+    return (
+        <section className="py-24 bg-tertiary dark:bg-secondary/40">
+            <div className="container mx-auto flex flex-col items-center">
+                <div className="flex flex-col items-center">
+                    <h2 className="h2 max-w-xl text-center mb-8">Prepared to turn your ideas into reality? I&apos;m here to help</h2>
+                </div>
+                <Link href="/contact">
+                    <Button>Contact Me</Button>
+                </Link>
+            </div>
+        </section>
+    );
+}
+
+export default Cta;
