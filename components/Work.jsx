b3f4fa14c824e96682171de61f5ad6ecdb329378
Ramanuj <75874452+Ramanuj-1729@users.noreply.github.com>
2023-11-15 17:29:42 +0530

    create project section

---
+++ components/Work.jsx
@@ -0,0 +1,96 @@
+'use client';
+import Link from "next/link";
+import { Button } from "./ui/button";
+import 'swiper/css';
+import 'swiper/css/pagination';
+import { Pagination } from 'swiper/modules';
+import ProjectCard from "@/components/ProjectCard";
+import { Swiper, SwiperSlide } from "swiper/react";
+
+const projectData = [
+    {
+        image: '/work/3.png',
+        category: 'react js',
+        name: 'Nexa Website',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla quam.',
+        link: '/',
+        github: '/'
+    },
+    {
+        image: '/work/4.png',
+        category: 'react js',
+        name: 'Nexa Website',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla quam.',
+        link: '/',
+        github: '/'
+    },
+    {
+        image: '/work/2.png',
+        category: 'next js',
+        name: 'Nexa Website',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla quam.',
+        link: '/',
+        github: '/'
+    },
+    {
+        image: '/work/1.png',
+        category: 'react js',
+        name: 'Nexa Website',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla quam.',
+        link: '/',
+        github: '/'
+    },
+    {
+        image: '/work/4.png',
+        category: 'react js',
+        name: 'Nexa Website',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla quam.',
+        link: '/',
+        github: '/'
+    },
+    {
+        image: '/work/2.png',
+        category: 'next js',
+        name: 'Nexa Website',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla quam.',
+        link: '/',
+        github: '/'
+    },
+    {
+        image: '/work/3.png',
+        category: 'react js',
+        name: 'Nexa Website',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla quam.',
+        link: '/',
+        github: '/'
+    },
+]
+
+const Work = () => {
+    return (
+        <section className="relative mb-12 xl:mb-48">
+            <div className="container mx-auto">
+                <div className="max-w-[400px] mx-auto xl:mx-0 text-center xl:text-left mb-12 xl:h-[400px] flex flex-col justify-center items-center xl:items-start">
+                    <h2 className="section-title mb-4">Latest Projects</h2>
+                    <p className="subtitle mb-8">Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+                    <Link href="/projects"><Button>All Projects</Button></Link>
+                </div>
+                {/* slider */}
+                <div className="xl:max-w-[1000px] xl:absolute right-0 top-0">
+                    <Swiper className="h-[560px]" slidesPerView={1} breakpoints={{ 640: { slidesPerView: 2, }, }} spaceBetween={30} modules={[Pagination]} pagination={{ clickable: true }}>
+                        {/* show only the first 4 projects */}
+                        {projectData.slice(0, 5).map((project, index) => {
+                            return (
+                                <SwiperSlide key={index}>
+                                    <ProjectCard project={project}></ProjectCard>
+                                </SwiperSlide>
+                            )
+                        })}
+                    </Swiper>
+                </div>
+            </div>
+        </section>
+    );
+}
+
+export default Work;
