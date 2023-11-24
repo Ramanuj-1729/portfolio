3f0882c4262930bc6536f8038656650b2a6de659
Ramanuj <75874452+Ramanuj-1729@users.noreply.github.com>
2023-11-25 00:32:50 +0530

    add backend for form, loader

--- components/Form.jsx
+++ components/Form.jsx
@@ -1,30 +1,66 @@
 'use client';
+import { useState } from "react";
 import { Button } from "./ui/button";
 import { Input } from "./ui/input";
 import { Textarea } from "./ui/textarea";
-import { User, MailIcon, ArrowRightIcon, MessageSquare } from "lucide-react";
+import { User, MailIcon, ArrowRightIcon, MessageSquare, CheckCircle, CheckCircle2 } from "lucide-react";
+import axios from "axios";
+import Loader from "./Loader";
 
 const Form = () => {
+    const initialFormData = {
+        name: '',
+        email: '',
+        message: '',
+    };
+    const [loading, setLoading] = useState(false);
+    const [success, setSuccess] = useState(false);
+    const [formData, setFormData] = useState(initialFormData);
+
+    const handleInputChange = (e) => {
+        const { name, value } = e.target;
+        setFormData((prev) => ({ ...prev, [name]: value }));
+    }
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        setLoading(true);
+        try {
+            await axios.post("https://ramanujapi.onrender.com/api/v1/queries", formData);
+            setSuccess(true);
+        } catch (error) {
+            alert(error);
+        } finally {
+            setLoading(false);
+            setTimeout(() => {
+                setSuccess(false);
+            }, 3000);
+            setFormData(initialFormData);
+        }
+    }
+
     return (
-        <form className="flex flex-col gap-y-4">
+        <form onSubmit={handleSubmit} className="form flex flex-col gap-y-4">
             {/* input */}
             <div className="relative flex items-center">
-                <Input type="name" id="name" placeholder="Name" />
+                <Input onChange={handleInputChange} value={formData.name} type="text" name="name" id="name" placeholder="Name" required />
                 <User className="absolute right-6" size={20} />
             </div>
             {/* input */}
             <div className="relative flex items-center">
-                <Input type="email" id="email" placeholder="Email" />
+                <Input onChange={handleInputChange} value={formData.email} type="email" name="email" id="email" placeholder="Email" required />
                 <MailIcon className="absolute right-6" size={20} />
             </div>
             {/* textarea */}
             <div className="relative flex items-center">
-                <Textarea placeholder="Type Your Message Here ..." />
+                <Textarea onChange={handleInputChange} value={formData.message} name="message" placeholder="Type Your Message Here ..." required />
                 <MessageSquare className="absolute top-4 right-6" size={20} />
             </div>
-            <Button className="flex items-center gap-x-1 max-w[166px]">
+            <Button type="submit" className="flex items-center gap-x-2 max-w[166px]">
                 Let&apos;s Talk
                 <ArrowRightIcon size={20} />
+                {loading && <Loader />}
+                {success && <CheckCircle2 />}
             </Button>
         </form>
     );
---
+++ components/Loader.jsx
@@ -0,0 +1,9 @@
+'use client';
+
+const Loader = () => {
+    return (
+        <div className="border-t-transparent border-solid animate-spin rounded-full border-gray2 border-4 h-6 w-6"></div>
+    );
+}
+
+export default Loader;
