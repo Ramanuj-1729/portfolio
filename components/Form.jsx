234e451afaf69b283578419036ba42b4e429dd8a
Ramanuj <75874452+Ramanuj-1729@users.noreply.github.com>
2023-11-25 00:52:33 +0530

    Fix axios post URL formatting

--- components/Form.jsx
+++ components/Form.jsx
@@ -26,7 +26,7 @@ const Form = () => {
         e.preventDefault();
         setLoading(true);
         try {
-            await axios.post(process.env.QUERY_URL, formData);
+            await axios.post(`${process.env.QUERY_URL}`, formData);
             setSuccess(true);
         } catch (error) {
             alert(error);
